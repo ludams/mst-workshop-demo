570f5eb32808e62f2cdf5ce7b1427b18bb79f80c
Lukas Dams <lukas.dams@protonmail.com>
2022-07-24 18:30:36 +0200

    Last ui fine tuning

--- src/components/TrackingTimer.tsx
+++ src/components/TrackingTimer.tsx
@@ -4,9 +4,11 @@ export const TrackingTimer: React.FC = () => {
   return (
     <div className={"container mb-4 bg-red-200"}>
       <h2 className={"heading"}>⏱ Tracking Timer</h2>
-      <div className={"p-2 rounded-xl bg-red-100"}>
-        <p>Currently running: </p>
-        <button>Start/Stop</button>
+      <div className={"flex flex-row items-center p-2 rounded-xl bg-red-100 w-full"}>
+        <button className={'flex items-center justify-center rounded-[50%] border-black border-1 w-8 h-8 mr-2 flex-shrink-0'}>
+          <span>{true ? '▶' : '■'}</span>
+        </button>
+        <p className={'overflow-hidden truncate'}>Attend Meeting</p>
       </div>
     </div>
   );
